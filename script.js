9cf48c4865a9905cb8456a6dfbc9da61644b8495
aneeqasghar20 <aneeqasghar20@gmail.com>
2021-07-05 04:30:28 +0500

    initial

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Fatima" && b=="6-07")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
